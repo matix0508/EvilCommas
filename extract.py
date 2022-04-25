f314a5e1980e7618cb890a5afc96d35da0c2f507
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-04-25 23:32:09 +0200

    make it pretty

--- extract.py
+++ extract.py
@@ -1,8 +1,21 @@
-from curses import raw
 import sys
 import numpy as np
 import pandas as pd
 import os
+
+def create_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+def write_to_file(dir, file_name, data, ext: str = "csv"):
+    with open(f"{dir}/{file_name}.{ext}", "w") as f:
+        f.write(data)
+
+def validate_question(copy: list, i: int, l: int):
+    print(f"[CURRENT CHOICE]::{copy[i:i+l]}")
+    if i+l < len(copy):
+        print(f"[NEXT CHOICE]::{copy[i+l]}")
+    return input("Is this a valid choice? (y/n/t/q)")
 
 class Extractor:
     def __init__(self, filename: str):
@@ -13,23 +26,28 @@ class Extractor:
     def __repr__(self):
         return f"{self.raw_data}"
 
+    def ask_column_name(self):
+        for i, col in enumerate(self.raw_data.columns):
+            print(f"{i}: {col}")
+        col_idx = int(input("Enter the column index: "))
+        return self.raw_data.columns[col_idx]
+
+    def good_question(self, copy, i: int, l: int, tab: list):
+        a = ", ".join(copy[i:i+l])
+        tab[a] = 1
+        for _ in range(i, i+l):
+            copy.pop(i)
+        return a
+
     def multichoice_col(self, col_name: str = None, ask_overwrite: bool = False):
-        if not os.path.exists("ans"):
-            os.mkdir("ans")
+        create_dir("ans")
         answers = []
         if col_name is None:
-            for i, col in enumerate(self.raw_data.columns):
-                print(f"{i}: {col}")
-            col_idx = int(input("Enter the column index: "))
-            col_name = self.raw_data.columns[col_idx]
-        self.raw_data[col_name].dtype
+            col_name = self.ask_column_name()
         if self.raw_data[col_name].dtype == int:
             return
         
-        print(os.path.exists(f"ans/{col_name}.csv"))
-        print(col_name)
         if os.path.exists(f"ans/{col_name}.csv"):
-            
             
             if ask_overwrite:
                 print("File already exists. Overwrite? (y/n/q)")
@@ -66,21 +84,12 @@ class Extractor:
                     l = 1
                     continue
                 if copy[i:i+l] in answers:
-                    a = ", ".join(copy[i:i+l])
-                    tab[a] = 1
-                    for sub in range(i, i+l):
-                        copy.pop(i)
+                    self.good_question(copy, i, l, tab)
                     l = 1
-                print(copy[i:i+l])
-                if i+l < len(copy):
-                    print(f"next question is: {copy[i+l]}")
-                q = input("Is this a valid question? (y/n/t)")
+                q = validate_question(copy, i, l)
                 if q == "y":
-                    a = ", ".join(copy[i:i+l])
+                    a = self.good_question(copy, i, l, tab)
                     answers.append(a)
-                    tab[a] = 1
-                    for sub in range(i, i+l):
-                        copy.pop(i)
                     l = 1
                 if q == "n":
                     l += 1
@@ -90,9 +99,9 @@ class Extractor:
                 if q == "q":
                     quit = True
                     break
-        with open(f"ans/{col_name}.csv", "w") as f:
-            f.write("; ".join(answers))
+
         self.output[col_name] = pd.DataFrame.from_dict(tab, orient="index")
+        write_to_file("ans", col_name, "; ".join(answers))
 
 
     def label_all(self):
@@ -109,9 +118,8 @@ class Extractor:
         for col in self.output:
             self.output[col].to_csv(f"output/{col}.csv")
 
-    
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 2:
         print("Usage: extract.py <filename>")
         exit(1)
@@ -120,3 +128,7 @@ if __name__ == "__main__":
     extractor.read_data()
     extractor.label_all()
     extractor.save_all()
+    
+
+if __name__ == "__main__":
+    main()
